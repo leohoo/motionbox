6b5bb8d630d2752e50fb504028f5dc51b62c589c
Wei Liu <lwsoftware@gmail.com>
2018-01-07 23:51:23 +0900

    if statement style

--- monitor-motion.sh
+++ monitor-motion.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-r=$(curl --head -s http://cubie:8081 | head -1 |grep "200 OK")
-if [ -z "$r" ]; then
+if ! curl --head -s http://cubie:8081 | head -1 | grep "200 OK" > /dev/null; then
   # motion not active
-
   disk=$(mount|grep "/media/disk-20g")
   if [ -z "$disk" ]; then
     sudo mount -L disk-20g /media/disk-20g
